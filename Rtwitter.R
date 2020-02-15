2e5217000dfb1c43c58a58312bcc3ad0fe98bf56
Meet Bhatnagar <46255664+MeetDarkPow@users.noreply.github.com>
2020-02-16 04:41:45 +0530

    Update Rtwitter.R

--- Rtwitter.R
+++ Rtwitter.R
@@ -1,63 +1,75 @@
-### Twitter Exploration ###
-api_key <- "2GeDrSFlkbzuM1IO8zgcY3FR3"
-api_secret_key <- "Vk2SIjYjSLNzwWSNJJfHGbxNyiYouryIfmYvGi7ZPmaP6XFz8V"
-access_token <- "1013338789580713984-MfcQumOgqWTZmovGHy2VvTli7t4jdG"
-access_token_secret <- "JasINJyQ0aBcjDR5SxXT7xhiIPjRknQZVmjFTAKRFowIM"
+install.packages("rtweet")
 
+install.packages("ggplot2")
+install.packages("rtweet")
+install.packages("ggplot2")
+install.packages("dplyr")
+install.packages("tidytext")
+install.packages("igraph")
+install.packages("ggraph")
+install.packages("widyr")
+install.packages("tidyr")
 
-token <- create_token(
-  app = "RtweetsExploration",
-  consumer_key = api_key,
-  consumer_secret = api_secret_key,
-  access_token = access_token,
-  access_secret = access_token_secret)
-
-## Page 1
-# searching tweets
 library(rtweet)
 library(tidytext)
 library(ggplot2)
 library(dplyr)
+library(dplyr)
+library(tidytext) 
+library(igraph)   
+library(ggraph)
+library(widyr)
+library(tidyr)
 
+# searching for #rstats tweets 
 rstats_tweets <- search_tweets(q="#rstats", retryonratelimit = T)
 head(rstats_tweets)
 
+# cleaning tweets
+# storing a cleaned tweet in a separate column named "stripped_text"
 rstats_tweets$stripped_text <- gsub("http.*","",rstats_tweets$text) 
 rstats_tweets$stripped_text <- gsub("https.*","",rstats_tweets$stripped_text)
 rstats_tweets$stripped_text <- gsub("#.*","",rstats_tweets$stripped_text)
 rstats_tweets$stripped_text <- gsub("@.*","",rstats_tweets$stripped_text)
 
-# Statistics for tweets between specified dates
+###################################################################################################################################
+
+# past tweets date
 rt <- as.Date(rstats_tweets$created_at)
 
-# DAILY
+# daily number of tweets
 daily <- as.Date("2020-02-11")
 daily_number_tweets <- sum(rt == daily, na.rm = TRUE)
 
-# WEEKLY
-since <- as.Date("2020-02-07")
-until <- as.Date("2020-02-13")
+# tweets between specific dates
+since <- as.Date("2020-02-10")
+until <- as.Date("2020-02-15")
 weekend <- as.Date(since:until, origin="1970-01-01")
 weekly_number_tweets <- c()
 for(i in 1:length(weekend)){
   weekly_number_tweets <- c(weekly_number_tweets, sum(rt == weekend[i], na.rm = TRUE))
 }
 weekly_frame <- data.frame(weekend, weekly_number_tweets)
+
+# ploting
 weekly_plot <- ggplot(data=weekly_frame, aes(x=weekend, y=weekly_number_tweets, group=1)) +
   geom_line(linetype="dashed", color="blue", size=1.2)+
   geom_point(color="red", size=3) +
-  ggtitle("#rstats tweets between 2020-02-07 and 2020-02-13") +
+  ggtitle("#rstats tweets between 2020-02-10 and 2020-02-15") +
   xlab("Days") + ylab("Number of Tweets")
+
+###################################################################################################################################
 
 # Name of account from where tweets were from
 head(rstats_tweets$screen_name)
 unique(rstats_tweets$screen_name)
 
-# Location from where tweets were from
+# location from where tweets were from
 users <- search_users("#rstats", n=500)
 
 length(unique(users$location))
 
+# ploting Twitter users - Unique Locations
 users %>%
   ggplot(aes(location)) +
   geom_bar() + coord_flip() +
@@ -65,6 +77,7 @@ users %>%
        y = "Location",
        title = "Twitter users - Unique Locations ")
 
+# ploting Twitter user's for tweeting #rstats in top 50 unique locations
 users %>%
   count(location, sort = TRUE) %>%
   mutate(location = reorder(location,n)) %>%
@@ -77,22 +90,16 @@ users %>%
        y = "Count",
        title = "Twitter user's for tweeting #rstats in top 20 unique locations ")
 
+###################################################################################################################################
+
 # text mining twitter data
 
-library(rtweet)   # load twitter library 
-library(ggplot2)  # plotting and pipes - tidyverse!
-library(dplyr)
-
-library(tidytext) # text mining library
-
-library(igraph)   # plotting packages
-library(ggraph)
-
-
+# selecting clean text
 rstats_tweets_clean <- rstats_tweets %>%
   select(stripped_text) %>%
   unnest_tokens(word, stripped_text)
 
+# ploting Count of unique words in tweets {Stop words present}
 rstats_tweets_clean %>%
   count(word, sort = TRUE) %>%
   top_n(15) %>%
@@ -105,17 +112,23 @@ rstats_tweets_clean %>%
        y= "Unique words",
        title = "Count of unique words in tweets")
 
-# stop_words 
+# searching what are stop_words 
 data("stop_words")
 head(stop_words)
 
+# number of rows, text has before removal of stop word
 nrow(rstats_tweets_clean)
 
+# removing stop words
 rstats_tweet_words <- rstats_tweets_clean %>%
   anti_join(stop_words)
 
+# number of rows, text has after removal of stop words
 nrow(rstats_tweet_words)
 
+# WE CAN LITERALLY SEE THE DIFFERENCE IN NUMBER OF ROWS
+
+# ploting Count of unique words in tweets {Stop words removed}
 rstats_tweet_words %>%
   count(word, sort = TRUE) %>%
   top_n(15) %>%
@@ -129,16 +142,19 @@ rstats_tweet_words %>%
        title = "Count of unique words found in tweets",
        subtitle = "Stop words removed from the list")
 
-library(widyr)
+###################################################################################################################################
 
+# Word Network
+
+# forming pair-words
 rstats_tweets_paired_words <- rstats_tweets %>%
   dplyr::select(stripped_text) %>%
   unnest_tokens(paired_words, stripped_text, token = "ngrams", n = 2)
-
+# count of pair-words
 rstats_tweets_paired_words %>%
   count(paired_words, sort = TRUE)
 
-library(tidyr)
+# separating pair-words into 2 columns
 rstats_tweets_separated_words <- rstats_tweets_paired_words %>%
   separate(paired_words, c("word1", "word2"), sep = " ")
 
@@ -150,6 +166,7 @@ rstats_words_counts <- rstats_tweets_filtered %>%
   count(word1, word2, sort = TRUE)
 head(rstats_words_counts)
 
+# ploting word network for #rstats
 rstats_words_counts %>%
   filter(n >= 30) %>%
   graph_from_data_frame() %>%
@@ -161,3 +178,4 @@ rstats_words_counts %>%
   labs(title = "Word Network: Tweets using the hashtag - Climate Change",
        subtitle = "Text mining twitter data ",
        x = "", y = "")
+###################################################################################################################################
