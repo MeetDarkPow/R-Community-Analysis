c86ae5983f6edf2034a17b6d28b5bbe3df85403b
Meet Bhatnagar <meet1708@gmail.com>
2020-03-05 23:52:27 +0530

    Update Test1.R

--- Test Solution/Test1.R
+++ Test Solution/Test1.R
@@ -1,3 +1,11 @@
+# FIRST FUNCTION
+
+install.packages("httr")
+install.packages("jsonlite")
+install.packages("httpuv")
+install.packages("gsubfn")
+install.packages("data.table")
+
 library(httr)
 library(jsonlite)
 library(httpuv)
@@ -8,10 +16,13 @@ dir.create("Filter_Search")
 
 repoExtract <- function(year){
   
+  # turning off the feature of R to turn multinomial variables,
+  # dummy variables in regression models and produce nice cross tables
   options(stringsAsFactors = FALSE)
   
   url <- "https://api.github.com"
   
+  # data extraction
   for(i in seq(1, 10, 1)){
     list_no <- paste("this.content", i, sep = "")
     path <- paste0("search/repositories?q=language:R&page=",as.character(i),"&per_page=100&sort=stars&order=desc")
@@ -21,9 +32,12 @@ repoExtract <- function(year){
     assign(list_no, this.content)
   }
   
+  # creating final list of all data extracted using API
   final <- list(this.content1, this.content2, this.content3, this.content4, this.content5,
                 this.content6, this.content7, this.content8, this.content9, this.content10)
   
+  # cleaning time format 
+  # storing repo_name and created_date as data.frame 
   datalist <- list()
   for(i in 1:10){
     repo_name <- final[[i]][["items"]][["name"]]
@@ -36,13 +50,19 @@ repoExtract <- function(year){
   
   big_data <- data.table::rbindlist(datalist)
   
+  # filtering per-month, per-year data according to user requirement 
   filter_repo <- subset(big_data, format(big_data$Created,"%Y")==year)
   filter_repo <- filter_repo[rev(order(filter_repo$Created, decreasing = TRUE)),]
   
+  # saving file with their respective year name
   saveRDS(filter_repo, paste0("Filter_Search/filter_repo_",year,".rds"))
 }
 
 # SECOND FUNCTION
+
+install.packages("tidyverse")
+install.packages("jsonlite")
+install.packages("data.table")
 
 library(tidyverse)
 library(data.table)
@@ -51,10 +71,13 @@ library(jsonlite)
 repoSave <- function(csv = "", json = ""){
   
   setwd("Filter_Search")
+  
+  # combining all .rds files and storing them as data.frame
   df <- list.files(pattern = ".rds") %>%
     map(readRDS) %>% 
     data.table::rbindlist()
   
+  # writing .csv and .json file
   dir.create("CSV_File")
   write.csv(df, file = paste0("CSV_File/",as.character(csv),".csv"), row.names = FALSE)
   
