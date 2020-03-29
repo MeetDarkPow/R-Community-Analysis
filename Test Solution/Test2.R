3cb2079e90a23e7b845c9cec47a75ed838991dbd
Meet Bhatnagar <meet1708@gmail.com>
2020-03-29 21:32:01 +0530

    Update Test2.R

--- Test Solution/Test2.R
+++ Test Solution/Test2.R
@@ -23,3 +23,26 @@ if(file.exists("Tweet_rstats_File/rstatsTweet.csv")){
 } else {
   write.csv(rstats_df, file = paste0("Tweet_rstats_File/rstatsTweet.csv"), row.names = F)
 }
+
+# updating the archive of #rstats tweets daily in the CSV file
+library(taskscheduleR)
+
+### remember to change the path/directory of searching #rstats code according to your system
+
+test2_script <- "C:\\Users\\MeetDarkPow\\Desktop\\R_Community_Explorer\\Test2.R"
+
+# Scheduled to run every day at 6:00 AM
+taskscheduler_create(
+  taskname = "Rtweet_taskscheduler",
+  rscript = test2_script,
+  schedule = "DAILY",
+  starttime = "06:00",
+  startdate = format(Sys.time(), "%d/%m/%Y"),
+  Rexe = file.path(Sys.getenv("R_Home"), "bin", "Rscript.exe")
+)
+
+# function to terminate the task schedular
+taskscheduler_stop("Rtweet_taskscheduler")
+
+# function to delete the task schedular
+taskscheduler_delete("Rtweet_taskscheduler")
