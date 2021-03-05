9bd08e7e22cc2b656682de998735ea3714cdfc1f
Meet Bhatnagar <meet1708@gmail.com>
2021-03-06 01:13:05 +0530

    Update StackExchange.R

--- StackExchange.R
+++ StackExchange.R
@@ -23,6 +23,8 @@ http_type(url)
 http_error(url)
 
 jsonURLparsed <- content(url, as="parsed")
+items <- jsonURLparsed$items
+length(items)
 
 modJson <- jsonURLparsed$items
 
@@ -38,23 +40,47 @@ df <- data.frame(ID=unlist(quesId), Title=unlist(quesTitle), View_Count=unlist(q
                  Answer=unlist(quesAnswered), Link=unlist(quesLink))
 
 
-
-url1 <- paste0("https://api.stackexchange.com/2.2/questions?key=", key, "&page=2&pagesize=100&fromdate=", fromDate, "&todate=", toDate, "&order=desc&sort=activity&access_token=", token, "&tagged=r&site=stackoverflow")
+pg <- 1
+url1 <- paste0("https://api.stackexchange.com/2.2/questions?key=", key, "&page=", pg, "&pagesize=100&fromdate=", fromDate, "&todate=", toDate, "&order=desc&sort=activity&access_token=", token, "&tagged=r&site=stackoverflow")
 url1 <- GET(url1)
 
-http_type(url1)
 http_error(url1)
 
 jsonURLparsed1111 <- content(url1, as="parsed")
-jsonURLparsed1111$has_more
-
-library(purrr)
-
-map_df()
+temp_modjson <- jsonURLparsed1111$items
+length(temp_modjson)
 
 
 
+temp_df <- data.frame(ID=integer(), Title=character(), View_Count=integer(), 
+                      Answer=logical(), Link=character())
 
+pg <- 1
+
+repeat{
+  url1 <- paste0("https://api.stackexchange.com/2.2/questions?key=", key, "&page=", pg, "&pagesize=100&fromdate=", fromDate, "&todate=", toDate, "&order=desc&sort=activity&access_token=", token, "&tagged=r&site=stackoverflow")
+  url1 <- GET(url1)
+  jsonURLparsed1111 <- content(url1, as="parsed")
+  temp_modjson <- jsonURLparsed1111$items
+  
+  if(length(temp_modjson)==0){
+    break
+  }
+  
+  len <- as.vector(1:length(temp_modjson))
+  
+  quesId <- lapply(len, function(x){temp_modjson[[x]][["question_id"]]})
+  quesTitle <- lapply(len, function(x){temp_modjson[[x]][["title"]]})
+  quesViewCount <- lapply(len, function(x){temp_modjson[[x]][["view_count"]]})
+  quesAnswered <- lapply(len, function(x){temp_modjson[[x]][["is_answered"]]})
+  quesLink <- lapply(len, function(x){temp_modjson[[x]][["link"]]})
+  
+  df <- data.frame(ID=unlist(quesId), Title=unlist(quesTitle), View_Count=unlist(quesViewCount), 
+                   Answer=unlist(quesAnswered), Link=unlist(quesLink))
+  
+  temp_df <- rbind(temp_df, df)
+  pg <- pg + 1
+}
 
 
 
