8d542c0de46511458c490f2cb38630ba0b1853c0
Meet Bhatnagar <meet1708@gmail.com>
2021-03-22 21:23:26 +0530

    Update Test_1.R

--- Test_Solution_2021/Test_1.R
+++ Test_Solution_2021/Test_1.R
@@ -14,17 +14,26 @@ token <- create_token(
   access_secret = acc_secret
 )
 
+if(!file.exists("Test_Solution_2021/rstats_data/rstats_Tweet.csv")){
+  empty_df <- data.frame(User_ID = character(), Status_ID = character(),
+                         Tweet_Date = character(), Tweet_Text = character(),
+                         Retweet_Count = integer(), Likes = integer())
+  write_as_csv(empty_df, file_name = "Test_Solution_2021/rstats_data/rstats_Tweet")
+}
+
+read_df <- read_twitter_csv("Test_Solution_2021/rstats_data/rstats_Tweet.csv")
+
 # searching for required #rstats tweets
-rtweet_data <- search_tweets(q="#rstats", since = Sys.Date()-1, 
-                             until = Sys.Date(), 
+rtweet_data <- search_tweets(q="#rstats", since = Sys.Date()-1,
+                             until = Sys.Date(),
                              retryonratelimit = TRUE, include_rts = FALSE)
 
 # creating a structured data frame
 rtweet_df <- data.frame(User_ID=rtweet_data$user_id, Status_ID=rtweet_data$status_id,
-                        Tweet_Date=rtweet_data$created_at, Tweet_Text=rtweet_data$text, 
+                        Tweet_Date=rtweet_data$created_at, Tweet_Text=rtweet_data$text,
                         Retweet_Count=rtweet_data$retweet_count, Likes = rtweet_data$favorite_count)
 
+temp_df <- rbind(read_df, rtweet_df)
+
 # saving #rstats tweets to a csv file
-write.table(rtweet_df, file = "Test_Solution_2021/rstats_data/rstats_Tweet.csv", sep = ",",
-            row.names = FALSE, col.names = !file.exists("rstats_Tweet.csv"), 
-            append = TRUE)
+write_as_csv(temp_df, file_name = "Test_Solution_2021/rstats_data/rstats_Tweet")
