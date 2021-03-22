7295727ca7a369e1aaff7bab4aa7f6c5d4a99324
Meet Bhatnagar <meet1708@gmail.com>
2021-03-22 08:35:24 +0530

    Update Test_1.R

--- Test Solution 2021/Test_1.R
+++ Test Solution 2021/Test_1.R
@@ -1,27 +1,29 @@
-creds <- read.csv('twitter_creds.csv', fileEncoding = "UTF-8-BOM")
-
-# store API keys
-api_key <- as.character(creds$API_key)
-api_secret_key <- as.character(creds$API_secret)
-acc_token <- as.character(creds$Access_token)
-acc_secret <- as.character(creds$Access_token_secret)
+# storing API keys
+api_key <- "Vasln0HaAY8pkXu7J2xQkBX8c"
+api_secret_key <- "h5ra23jvrY8nq7vkUBld1mXXYwjsKR5iyNoH2CATUNKO1rdzq3"
+acc_token <- "1013338789580713984-2oRZj3Btzvgl0yZqJ8Wfr0GEmTzW34"
+acc_secret <- "92oQXpMxvdNI7gwIAji2ZfEdN905eGudFoeNWd4sb6Wnn"
 
 library(rtweet)
 
 token <- create_token(
   app = "RtweetsExploration",
   consumer_key = api_key,
-  consumer_secret = api_secret_key
+  consumer_secret = api_secret_key,
+  access_token = acc_token,
+  access_secret = acc_secret
 )
 
-rtweet_data <- search_tweets(q="#rstats", since = Sys.Date()-1, 
-                             until = Sys.Date(), 
+# searching for required #rstats tweets
+rtweet_data <- search_tweets(q="#rstats", since = Sys.Date()-2, 
+                             until = Sys.Date()-1, 
                              retryonratelimit = TRUE, include_rts = FALSE)
 
 rtweet_df <- data.frame(User_ID=rtweet_data$user_id, Status_ID=rtweet_data$status_id,
                         Tweet_Date=rtweet_data$created_at, Tweet_Text=rtweet_data$text, 
                         Retweet_Count=rtweet_data$retweet_count, Likes = rtweet_data$favorite_count)
 
-write.table(rtweet_df, file = "rstats_Tweet.csv", sep = ",",
+# saving #rstats tweets to a csv file
+write.table(rtweet_df, file = "data/rstats_Tweet.csv", sep = ",",
             row.names = FALSE, col.names = !file.exists("rstats_Tweet.csv"), 
             append = TRUE)
